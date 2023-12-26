aa235eeca90c8e3bbdd391bed08cd2a0d267cab7
Gabriel Weyer <gabrielweyer@gmail.com>
2023-12-26 12:34:41 +1100

    Use styleUrl string instead of styleUrls array
    [deploy-pr]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrl: './app.component.scss',
   standalone: true,
   imports: [FooterComponent]
 })
